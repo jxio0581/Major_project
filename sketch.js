068196c1144a6439466ce36f75e4caa31b01e6ea
jxio0581 <jxio0581@uni.sydney.edu.au>
2024-11-13 21:16:45 +1100

    Final Animation Effect Code
    add smooth animation

--- sketch.js
+++ sketch.js
@@ -1,6 +1,6 @@
 let img;
 let music;
-let numSegments = 50;
+let numSegments = 80;
 let segments = [];
 let button;
 let sliderVolume;
@@ -15,9 +15,10 @@ function preload() {
 function setup() {
   // creat canvas to contain both img and button
   createCanvas(windowWidth, windowHeight - 50);
+  // lower speed
+  frameRate(30);
   // analyse music
   fft = new p5.FFT(0.9, 256); 
-
   // The technique about the buttons was learned from https://www.youtube.com/watch?v=Pn1g1wjxl_0&list=PLRqwX-V7Uu6aFcVjlDAkkGIixw70s7jpW&index=1
   // Create the play button and set the mouse click event
   button = createButton("Play");
@@ -34,9 +35,8 @@ function draw() {
   // use buttons to control music
   music.setVolume(sliderVolume.value());
   music.rate(sliderRate.value());
-  placeButton()
-  
-  // 
+  placeButton()  
+  // loop array to draw rect
   for (const segment of segments) {
     let frequency = fft.analyze();  //hold data
     let MusicValue = frequency[int(random(frequency.length))];  // hold a int value from music data
@@ -98,21 +98,46 @@ class ImageSegment {
     this.y = y;
     this.width = width;
     this.height = height;
-    this.color = color;
+    this.color = this.createColor(color);  // Make sure the color is p5.js recognizable. This code was added after an error was reported and debugged via chatGPT.
+    // set two status for smoothing
+    this.currentWidth = width;
+    this.currentHeight = height;
+    this.targetWidth = width;
+    this.targetHeight = height;
+    this.currentColor = this.color; 
+    this.targetColor = this.color;
+  }
+// Make sure the color is p5.js recognizable. This code was added after an error was reported and debugged via chatGPT.
+  createColor(c) {
+    if (c instanceof p5.Color) {
+      return c;
+    } else {
+      return color(c[0], c[1], c[2]);
+    }
   }
   // draw dynamic rect by music data
-  draw(MusicValue) {
-    // use map to set the new size of rect
-    let changeSize = map(MusicValue, 0, 255, this.width * 1, this.width * 1.2);
-    // change r,g,b to change color
-    let changeColor = color(
-      red(this.color) * (1 + MusicValue / 255),
-      green(this.color) * (1 + MusicValue / 255),
-      blue(this.color) * (1 + MusicValue / 255)
-    );
-    fill(changeColor);
+  draw(energy) {
+    // use lerp() to smooth size
+    // The technique about the lerp was learned from https://p5js.org/search/?term=lerp
+    this.targetWidth = map(energy, 0, 255, this.width * 0.9, this.width * 2.4);
+    this.targetHeight = map(energy, 0, 255, this.height * 0.9, this.height * 2.4);
+    this.currentWidth = lerp(this.currentWidth, this.targetWidth, 0.15);
+    this.currentHeight = lerp(this.currentHeight, this.targetHeight, 0.15);
+    // update color
+    let brightFactor = 1 + energy / 255;
+    let r = red(this.color) * brightFactor;
+    let g = green(this.color) * brightFactor;
+    let b = blue(this.color) * brightFactor;
+    // make sure color is valid. This code was added after an error was reported and debugged via chatGPT.
+    r = constrain(r, 0, 255);
+    g = constrain(g, 0, 255);
+    b = constrain(b, 0, 255);
+    this.targetColor = color(r, g, b);
+    // use lerpColor() to smooth color
+    this.currentColor = lerpColor(this.currentColor, this.targetColor, 0.15);
+    fill(this.currentColor);
     noStroke();
-    rect(this.x + (this.width - changeSize) / 2, this.y + (this.height - changeSize) / 2, changeSize, changeSize);
+    rect(this.x + (this.width - this.currentWidth) / 2, this.y + (this.height - this.currentWidth) / 2, this.currentWidth, this.currentHeight);
   }
 }
 // Update image to fit window size
